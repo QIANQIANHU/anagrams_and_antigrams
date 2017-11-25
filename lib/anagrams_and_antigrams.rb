0ac721c546655ce9f535e57c5a7a79cc4fb5c5f6
Qianqian Hu <qianhu@pindous.com>
2017-11-25 13:10:20 -0800

    check if words are antigrams or not

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,11 +1,13 @@
 def anagrams_and_antigrams?(string1, string2)
 
   if !word_contains_vowel?(string1, string2)
-    "You need to input actual words!"# case 4: if word contains a vowel
+    "You need to input actual words!" # case 4: if word contains a vowel
   elsif anagrams?(string1, string2) & (palindrome?(string1, string2))
-    "These words are palindrome."  # case 3: simple anagrams or not
+    "These words are palindrome."  # case 3: if words are anagrams or not
   elsif anagrams?(string1, string2)
-     "These words are anagrams."  # case 1,2: simple anagrams or not
+     "These words are anagrams."  # case 1,2: if words are anagrams or not
+  elsif antigrams?(string1, string2)
+     "These words have no letter matches and are antigrams." # case 5: if words are antigrams or not
   end
 
 end
@@ -28,4 +30,20 @@ def word_contains_vowel?(string1, string2)
   (processed_string1.include?("a") | processed_string1.include?("e") | processed_string1.include?("i") | processed_string1.include?("o") | processed_string1.include?("u") ) &
   (processed_string2.include?("a") | processed_string2.include?("e") | processed_string2.include?("i") | processed_string2.include?("o") |
   processed_string2.include?("u") )
+end
+
+def antigrams?(string1, string2)
+  string1_array = string1.downcase.split("")
+  string2_array = string2.downcase.split("")
+  string1_array.each do |letter|
+    if string2_array.include?(letter)
+      return false
+    end
+  end
+  string2_array.each do |letter|
+    if string1_array.include?(letter)
+      return false
+    end
+  end
+  return true
 end
