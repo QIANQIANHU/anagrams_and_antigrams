8111e6917c62c86f1e5c2f9b241a6081cf7d4d8c
Qianqian Hu <qianhu@pindous.com>
2017-11-25 10:03:26 -0800

    check two words with different cases are anagrams or not

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,3 +1,8 @@
+def anagrams_and_antigrams?(string1, string2)
+  # case 1: simple anagrams or not
+  anagrams?(string1, string2)
+end
+
 def sorted_chars_from_string(string)
   string.downcase.gsub(/\s/, '').chars.sort
 end
@@ -5,3 +10,5 @@ end
 def anagrams?(string1, string2)
   sorted_chars_from_string(string1) == sorted_chars_from_string(string2)
 end
+
+# def palindrome(string)
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -3,8 +3,14 @@ require('anagrams_and_antigrams')
 
 # require('pry')
 
-describe('#anagrams?') do
+describe('#anagrams_and_antigrams') do
   it("check two words are anagrams or not") do
-    expect(anagrams?("cat", "tac")).to(eq(true))
+    expect(anagrams_and_antigrams?("cat", "tac")).to(eq(true))
   end
+  it("check two words with different cases are anagrams or not") do
+    expect(anagrams_and_antigrams?("Tea", "Eat")).to(eq(true))
+  end
+  # it("check anagrams of two words are palindrome or not") do
+  #   expect(anagrams?("cat", "tac")).to(eq(true))
+  # end
 end
